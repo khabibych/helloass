dd535128338fb2ba521daece17076c5ee044239d
gadzh <azizgadzhiev97@yandex.ru>
2024-10-10 00:55:38 +0300

    helloass
    Signed-off-by: gadzh <azizgadzhiev97@yandex.ru>

--- src/Main.java
+++ src/Main.java
@@ -62,6 +62,6 @@ public class Main {
         var totallyNew = totally + 94;
         System.out.println(totallyNew);
         var hoursNew = totalHours / totallyNew;
-        System.out.println("Если в компании работает" + " " + totallyNew + " " + "человека, то всего" + " " + hoursNew + " " + "часов работы может быть поделено между сотрудниками");
+        System.out.println("Если в компании работает" + " " + totallyNew + " " + "человека, то всего" + " " + hoursNew + " " + "часов работы может быть поделено между сотрудниками!");
     }
 }
